2aa9e9d3c7b81b97e02c840013df3098e9c64857
Corina Neagu <neagucorina22@stud.ase.ro>
2023-11-24 21:43:09 +0200

    tested the operators (without the conditional one and the PRE INCREMENTATION one

--- MyProject/Main.cpp
+++ MyProject/Main.cpp
@@ -40,8 +40,60 @@ int main() {
 	e5 = 2 + e3; //e5=2+30
 	cout << endl << e5.getDuration(); //32
 	cout << endl << e3.getDuration(); //30
+	cout << endl;
 
+	//calling operator++ inside the class
+	//POST INCREMENTATION
+	cout << endl;
+	cout << endl << e3.getDuration(); //30
+	cout << endl << e2.getDuration(); //90
+	e3 = e2++; //e3 takes the duration of e2, then e2 duration increments by 1
+	cout << endl << e3.getDuration(); //30
+	cout << endl << e2.getDuration(); //91
 
+	//calling operator++ outside the class
+	//PRE INCREMENTATION
+	//cout << endl;
+	//cout << endl << e3.getDuration();
+	//cout << endl << e2.getDuration();
+	//e3 = ++e2; //first increment the duration of s2 by 1, then give e3 this duration
+	//cout << endl << e3.getDuration();
+	//cout << endl << e2.getDuration();
 
+	cout << endl;
+	//calling the index operator
+	try {
+		cout << endl << e2[2] << endl; //play 0123
+	}
+	catch (exception ex) {
+		cout << ex.what();
+	}
+
+	cout << endl;
+	//calling the cast operator 
+	cout << "CAST";
+	Event e7("Movie", 120);
+	int duration = e7;
+
+	cout << endl;
+
+	//calling the ! operator
+	cout << e4.getIsSoldOut();
+	cout << endl;
+	cout << !e4;
+
+	//1-true; 0-false
+
+	//calling the <= operator
+
+	//calling the == operator
+	if (e1 == e2) { //not equal
+		cout << endl << "They are equal.";
+	}
+	else {
+		cout << endl << "They are not equal.";
+	}
+
+	
 	return 0;
 }
