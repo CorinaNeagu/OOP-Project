0c799013a252100250052aab20d992df980fb271
Corina Neagu <neagucorina22@stud.ase.ro>
2023-11-26 12:16:26 +0200

    tested the first part of the 3rd class

--- MyProject/Main.cpp
+++ MyProject/Main.cpp
@@ -9,20 +9,20 @@ int main() {
 	//testing the first class - Event
 	
 	////creating objects 
-	//Event e1("Concert", 30);
-	//cout << e1;
+	/*Event e1("Concert", 30);
+	cout << e1;
 
-	//Event e2("Play", 90);
-	//cout << e2;
+	Event e2("Play", 90);
+	cout << e2;*/
 
 	////calling the copy constructor
 	//Event e3 = e1;
 	//cout << e3;
 
-	//int seats[4] = { 10, 20, 30, 40 };
+	/*int seats[4] = { 10, 20, 30, 40 };
 
-	//Event e4("Concert", 90, seats, 4, false);
-	//cout << e4;
+	Event e4("Concert", 90, seats, 4, false);
+	cout << e4;*/
 
 	////calling operator=
 	//Event e5;
@@ -98,91 +98,132 @@ int main() {
 	//	cout << endl << "They are not equal.";
 	//}
 
-	//cin >> e2;;
+	//cin >> e2; 
+	//separat
+	//cin >> e4;
+
+
+//testing for 2nd class - Location
+
+	////creating objects
+	Location l1("Baneasa", "Alexandru", VIP);
+	cout << l1;
+
+	int seats[] = { 10,20,30,40,50,60,70,80,90,100};
+	Location l2("Aviatorilor", "Ion", 10, seats, 10);
+	cout << l2;
+
+	Location l3("Parcul Tei", "Alexandru", 20, seats, 40);
+	cout << l3;
+	cin >> l3;
+
+	Location l4("Arena Nationala", "Dorian", NORMAL);
+
+	//calling the copy constructor
+	Location l5 = l2;
+	cout << l2;
+
+	//calling operator=
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 	//testing the third class - Ticket
 
 	//creating objects
-	Ticket t1("Swan Lake", 80, false); //0 = false; 1= true;
-	cout << t1;
-	cout << endl;
-	//cin >> t1;
+	//Ticket t1("Swan Lake", 80, false); //0 = false; 1= true;
+	//cout << t1;
+	//cout << endl;
+	////cin >> t1;
 
-	cout << endl;
-	Ticket t2("Nutcracker", "Held at National Opera House", 100, true);
-	cout << t2;
-	cout << endl;
-	//cin >> t2;
+	//cout << endl;
+	//Ticket t2("Nutcracker", "Held at National Opera House", 100, true);
+	//cout << t2;
+	//cout << endl;
+	////cin >> t2;
 
-	//calling the operator=
-	Ticket t3;
-	t3 = t2;
-	cout << t3;
+	////calling the operator=
+	//Ticket t3;
+	//t3 = t2;
+	//cout << t3;
 
-	//calling the copy constructor
-	Ticket t4 = t2;
-	cout << t4;
-	cout << endl;
+	////calling the copy constructor
+	//Ticket t4 = t2;
+	//cout << t4;
+	//cout << endl;
 
-	Ticket t5("Hamlet", "Held on Lipscani", 200, false);
-	t5.printInfo();
-	
-	
-	////operator+ inside the class
+	//Ticket t5("Hamlet", "Held on Lipscani", 200, false);
+	//t5.printInfo();
+	//
+	//
+	//////operator+ inside the class
+	////cout << endl << t3.getPrice(); //100
+	////cout << endl << t2.getPrice(); //100
+	////t3 = t2 + 60; // t3=160
+	////cout << endl << t3.getPrice(); //160
+	////cout << endl << t2.getPrice(); //100
+	//
+
+	////operator+ outside the class
 	//cout << endl << t3.getPrice(); //100
 	//cout << endl << t2.getPrice(); //100
-	//t3 = t2 + 60; // t3=160
+	//t3 = 60 + t2; // t3=160
 	//cout << endl << t3.getPrice(); //160
 	//cout << endl << t2.getPrice(); //100
-	
+	//cout << endl;
 
-	//operator+ outside the class
-	cout << endl << t3.getPrice(); //100
-	cout << endl << t2.getPrice(); //100
-	t3 = 60 + t2; // t3=160
-	cout << endl << t3.getPrice(); //160
-	cout << endl << t2.getPrice(); //100
-	cout << endl;
+	//////operator ++ inside the class POST INCREMENTATION - error
+	////cout << endl << t2.getPrice(); //100
+	////cout << endl << t3.getPrice(); //160
+	////t2 = t3++; //t2 takes the price of t3, then t3 increments by 1
+	////cout << endl << t2.getPrice(); //160
+	////cout << endl << t3.getPrice(); //161
 
-	////operator ++ inside the class POST INCREMENTATION - error
+
+	//////operator ++ outside the class PRE INCREMENTATION
 	//cout << endl << t2.getPrice(); //100
 	//cout << endl << t3.getPrice(); //160
-	//t2 = t3++; //t2 takes the price of t3, then t3 increments by 1
-	//cout << endl << t2.getPrice(); //160
-	//cout << endl << t3.getPrice(); //161
+	//t3 = ++t2; //increment t2 by 1, then give this price to t3
+	//cout << endl << t2.getPrice(); //101
+	//cout << endl << t3.getPrice(); //101
+	//cout << endl;
 
 
-	////operator ++ outside the class PRE INCREMENTATION
-	cout << endl << t2.getPrice(); //100
-	cout << endl << t3.getPrice(); //160
-	t3 = ++t2; //increment t2 by 1, then give this price to t3
-	cout << endl << t2.getPrice(); //101
-	cout << endl << t3.getPrice(); //101
-	cout << endl;
+	////
+	//////calling the index operator
+	//try {
+	//	cout << endl << t2[2] << endl; //nutcracker 0123456789
+	//}
+	//catch (exception ex) {
+	//	cout << ex.what();
+	//}
+	//cout << endl;
 
+	//////calling the negation operator
+	//cout << t2.getIsVIP();
+	//cout << endl;
+	//cout << !t2;
 
-	//
-	////calling the index operator
-	try {
-		cout << endl << t2[2] << endl; //nutcracker 0123456789
-	}
-	catch (exception ex) {
-		cout << ex.what();
-	}
-	cout << endl;
-
-	////calling the negation operator
-	cout << t2.getIsVIP();
-	cout << endl;
-	cout << !t2;
-
-	////calling the == operator
-	if (t2 == t5) { //not equal
-		cout << endl << "They are equal.";
-	}
-	else {
-		cout << endl << "They are not equal.";
-	}
+	//////calling the == operator
+	//if (t2 == t5) { //not equal
+	//	cout << endl << "They are equal.";
+	//}
+	//else {
+	//	cout << endl << "They are not equal.";
+	//}
 
 
 	return 0;
