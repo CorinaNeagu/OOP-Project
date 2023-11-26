031e17e91e708a46a7c6e22947857e865539b716
Corina Neagu <neagucorina22@stud.ase.ro>
2023-11-26 15:07:53 +0200

    added the conditional operators for classes Ticket & Event; tested all other operators (except +, ++, -, --) for class Location

--- MyProject/Main.cpp
+++ MyProject/Main.cpp
@@ -29,7 +29,7 @@ int main() {
 	//e5 = e4;
 	//cout << e5;
 
-	////operator+ inside the class
+	////operator+ inside the class 
 	//cout << endl;
 	//cout << e3.getDuration(); //30
 	//cout << endl << e5.getDuration(); //90
@@ -88,7 +88,13 @@ int main() {
 
 	////1-true; 0-false
 
-	////calling the <= operator
+	////calling the < operator
+	/*if (e3 < 90) {
+		cout << endl << "Event 3 is shorter than 90 minutes.";
+	}
+	else {
+		cout << endl << "Event 3 is longer than 90 minutes.";
+	}*/
 
 	////calling the == operator
 	//if (e1 == e2) { //not equal
@@ -103,39 +109,78 @@ int main() {
 	//cin >> e4;
 
 
+
+
+
 //testing for 2nd class - Location
 
 	////creating objects
-	Location l1("Baneasa", "Alexandru", VIP);
+	Location l1("Baneasa", "Alexandru Tanase", VIP);
 	cout << l1;
 
 	int seats[] = { 10,20,30,40,50,60,70,80,90,100};
-	Location l2("Aviatorilor", "Ion", 10, seats, 10);
+	Location l2("Aviatorilor", "Ion Daniel", 10, seats, 10);
 	cout << l2;
 
-	Location l3("Parcul Tei", "Alexandru", 20, seats, 40);
+	Location l3("Parcul Tei", "Alexandru Popescu", 20, seats, 40);
 	cout << l3;
-	cin >> l3;
+	//cin >> l3;
 
-	Location l4("Arena Nationala", "Dorian", NORMAL);
+	Location l4("Arena Nationala", "Maria Comanescu", NORMAL);
+	cout << l4;
+
+	Location l7("Timisoara", "Cristina Alexandrescu", 50, seats, 60);
 
 	//calling the copy constructor
 	Location l5 = l2;
 	cout << l2;
 
 	//calling operator=
+	Location l6;
+	l6 = l4;
+	cout << l6;
 
 
+	////calling the index operator
+	try {
+		cout << endl << l1[2] << endl; // alexandru 012345678 //e
+	}
+	catch (exception ex) {
+		cout << ex.what();
+	}
 
+	//calling the ! operator
+	if (!l2) { //l2 has 10 rows
+		cout << "Location 2 has more than 15 rows";
+	}
+	else {
+		cout << "Location 2 does not have 15 or more rows. It has only 10.";
+	}
 
+	cout << endl;
 
+	if (!l3) { //l3 has 20 rows
+		cout << "Location 3 has more than 15 rows. ";
+	}
+	else {
+		cout<< "Location 2 does not have 15 or more rows. It has only 10.";
+	}
 
+	//calling the == operator
+	if (l2 == l3) { //not equal
+			cout << endl << "They are equal.";
+		}
+		else {
+			cout << endl << "They are not equal.";
+		}
 
-
-
-
-
-
+	//calling the > operator
+	if (l2 > 5) {
+		cout << endl << "Location 2 has more rows than just 5.";
+	}
+	else {
+		cout << endl << "Location 2 has less than 5 rows.";
+	}
 
 
 
@@ -169,7 +214,7 @@ int main() {
 	//t5.printInfo();
 	//
 	//
-	//////operator+ inside the class
+	//////operator+ inside the class 
 	////cout << endl << t3.getPrice(); //100
 	////cout << endl << t2.getPrice(); //100
 	////t3 = t2 + 60; // t3=160
@@ -216,6 +261,14 @@ int main() {
 	//cout << t2.getIsVIP();
 	//cout << endl;
 	//cout << !t2;
+
+	//calling the > operator
+	/*if (t2 > 50) {
+		cout << endl << "Ticket 2 costs more than 50 lei.";
+	}
+	else {
+		cout << endl << "Ticket 2 costs less than 50 lei";
+	}*/
 
 	//////calling the == operator
 	//if (t2 == t5) { //not equal
